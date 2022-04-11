e1fa5fa56ecc7e810aef7109a2e01c992489b90a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-04-11 11:02:12 +0900

    added Button variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,23 +3,36 @@ import styled from "styled-components";
 
 type Props = {
   children: string;
+  variant?: "primary" | "secondary" | "tertiary";
+  className?: string;
 };
 
-const Button = ({ children }: Props) => {
-  return <StyledButton>{children}</StyledButton>;
+const Button = ({ children, className, variant }: Props) => {
+  return <StyledButton className={className} variant={variant}>{children}</StyledButton>;
 };
 
 export default Button;
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<Props>`
   border-radius: 10px;
   text-align: center;
   font-size: 0.8125rem;
   font-weight: 600;
   line-height: 18px;
-  padding: 0.375rem 1rem;
-  background-color: ${(props) => props.theme.colors.light.dark};
-  color: ${(props) => props.theme.colors.tertiary.dark};
+  padding: 0.625rem 1rem;
+  background-color: ${(props) => {
+    switch (props.variant) {
+      case "primary":
+        return props.theme.colors.primary;
+      case "secondary":
+        return props.theme.colors.secondary.light;
+      case "tertiary":
+        return props.theme.colors.tertiary.light;
+      default:
+        return props.theme.colors.grey.dark;
+    }
+  }};
+  color: ${(props) => props.theme.colors.light.dark};
   transition: 0.35s ease-in-out;
 
   &.active {
